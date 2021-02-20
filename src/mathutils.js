f1e0b43050f78c452c1422710ceb968da358f115
ssc1729 <ssc1729@outlook.com>
2021-02-21 00:23:47 +0530

    Negative angle fix

--- src/mathutils.js
+++ src/mathutils.js
@@ -10,6 +10,24 @@ class Complex {
     }
 }
 
+class Vector3Helpers {
+    static angleBetweenVectors(vector1, vector2, planeNormal) {
+        let angle = THREE.Math.radToDeg(vector1.angleTo(vector2));
+
+        if (true) {
+            let crossProduct = new THREE.Vector3();
+
+            crossProduct.crossVectors(vector1, vector2);
+            if (crossProduct.dot(planeNormal) < 0) {
+                angle *= -1;
+            }
+        }
+
+        return angle;
+    }
+}
+
 export {
-    Complex
+    Complex,
+    Vector3Helpers
 };
