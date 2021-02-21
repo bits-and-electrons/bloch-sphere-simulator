d08852266cd68361c9d5fe59519d7e69f4e89be7
Sai Seshu Chadaram <78927110+ssc1729@users.noreply.github.com>
2021-02-21 23:29:46 +0530

    Quantum state precision fix (#13)

--- src/quantum/quantum_state.js
+++ src/quantum/quantum_state.js
@@ -8,35 +8,21 @@ class QuantumState {
     }
 
     load() {
-        this.alpha = Math.cos(THREE.Math.degToRad(this.theta) / 2);
+        this.alpha = parseFloat(Math.cos(THREE.Math.degToRad(this.theta) / 2).toFixed(4));
         this.beta = new Complex(
-            Math.cos(THREE.Math.degToRad(this.phi)) * Math.sin(THREE.Math.degToRad(this.theta) / 2),
-            Math.sin(THREE.Math.degToRad(this.phi)) * Math.sin(THREE.Math.degToRad(this.theta) / 2)
+            parseFloat((Math.cos(THREE.Math.degToRad(this.phi)) * Math.sin(THREE.Math.degToRad(this.theta) / 2)).toFixed(4)),
+            parseFloat((Math.sin(THREE.Math.degToRad(this.phi)) * Math.sin(THREE.Math.degToRad(this.theta) / 2)).toFixed(4))
         );
 
-        this.x = Math.sin(THREE.Math.degToRad(this.theta)) * Math.cos(THREE.Math.degToRad(this.phi));
-        this.y = Math.sin(THREE.Math.degToRad(this.theta)) * Math.sin(THREE.Math.degToRad(this.phi));
-        this.z = Math.cos(THREE.Math.degToRad(this.theta));
-    }
-
-    round() {
-        this.theta = parseFloat(this.theta.toFixed(4));
-        this.phi = parseFloat(this.phi.toFixed(4));
-
-        this.alpha = parseFloat(this.alpha.toFixed(4));
-        this.beta = parseFloat(this.beta.toFixed(4));
-
-        this.x = parseFloat(this.x.toFixed(4));
-        this.y = parseFloat(this.y.toFixed(4));
-        this.z = parseFloat(this.z.toFixed(4));
+        this.x = parseFloat((Math.sin(THREE.Math.degToRad(this.theta)) * Math.cos(THREE.Math.degToRad(this.phi))).toFixed(4));
+        this.y = parseFloat((Math.sin(THREE.Math.degToRad(this.theta)) * Math.sin(THREE.Math.degToRad(this.phi))).toFixed(4));
+        this.z = parseFloat(Math.cos(THREE.Math.degToRad(this.theta)).toFixed(4));
     }
 
     update(theta, phi) {
-        this.theta = theta;
-        this.phi = phi;
-
+        this.theta = parseFloat(theta.toFixed(4));
+        this.phi = parseFloat(phi.toFixed(4));
         this.load();
-        this.round();
     }
 }
 
