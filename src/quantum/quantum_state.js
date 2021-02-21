78aa9608a4cddb5d64d0d6e9a703d6c1ddfd28a2
ssc1729 <ssc1729@outlook.com>
2021-02-21 16:32:31 +0530

    Negative sign fix

--- src/quantum/quantum_state.js
+++ src/quantum/quantum_state.js
@@ -19,10 +19,24 @@ class QuantumState {
         this.z = Math.cos(THREE.Math.degToRad(this.theta));
     }
 
+    round() {
+        this.theta = parseFloat(this.theta.toFixed(4));
+        this.phi = parseFloat(this.phi.toFixed(4));
+
+        this.alpha = parseFloat(this.alpha.toFixed(4));
+        this.beta = parseFloat(this.beta.toFixed(4));
+
+        this.x = parseFloat(this.x.toFixed(4));
+        this.y = parseFloat(this.y.toFixed(4));
+        this.z = parseFloat(this.z.toFixed(4));
+    }
+
     update(theta, phi) {
         this.theta = theta;
         this.phi = phi;
+
         this.load();
+        this.round();
     }
 }
 
